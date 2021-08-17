7d4abe50b257e6413161c73022a5140748c49eb5
webpointdev <webtiger1106@gmail.com>
2021-08-16 23:36:19 -0700

    second commit

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -3,14 +3,46 @@
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col bg-dark text-white">
-					<a class="navbar-brand">SPORT STORE</a>
+					<a class="navbar-brand">SPORTS STORE</a>
 				</div>
 			</div>
 		</div>
 		<div class="m-2">
 			<div class="form-group m-2">
 				<label>Name</label>
-				<input v-model="name" class="form-control" />
+				<input v-model="$v.order.name.$model" class="form-control " />
+				<validation-error v-bind:validation="$v.order.name" />
+			</div>
+		</div>
+		<div class="m-2">
+			<div class="form-group m-2">
+				<label>Email</label>
+				<input v-model="$v.order.email.$model" class="form-control " />
+				<validation-error v-bind:validation="$v.order.email" />
+			</div>
+		</div>
+		<div class="m-2">
+			<div class="form-group m-2">
+				<label>Address</label>
+				<input
+					v-model="$v.order.address.$model"
+					class="form-control "
+				/>
+				<validation-error v-bind:validation="$v.order.address" />
+			</div>
+		</div>
+		<div class="m-2">
+			<div class="form-group m-2">
+				<label>City</label>
+				<input v-model="$v.order.city.$model" class="form-control " />
+				<validation-error v-bind:validation="$v.order.city" />
+			</div>
+		</div>
+		<div class="m-2">
+			<div class="form-group m-2">
+				<label>Zip</label>
+				<input v-model="$v.order.zip.$model" class="form-control " />
+				<validation-error v-bind:validation="$v.order.zip" />
 			</div>
 		</div>
 		<div class="text-center">
@@ -23,17 +55,44 @@
 		</div>
 	</div>
 </template>
-
 <script>
+import { required, email } from "vuelidate/lib/validators";
+import ValidationError from "./ValidationError";
+import { mapActions } from "vuex";
 export default {
+	components: { ValidationError },
 	data: function() {
 		return {
-			name: null,
+			order: {
+				name: null,
+				email: null,
+				address: null,
+				city: null,
+				zip: null,
+			},
 		};
 	},
+	validations: {
+		order: {
+			name: { required },
+			email: { required, email },
+			address: { required },
+			city: { required },
+			zip: { required },
+		},
+	},
 	methods: {
-		submitOrder() {
-			// todo: save order
+		...mapActions({
+			storeOrder: "storeOrder",
+			clearCart: "cart/clearCartData",
+		}),
+		async submitOrder() {
+			this.$v.$touch();
+			if (!this.$v.$invalid) {
+				let order = await this.storeOrder(this.order);
+				this.clearCart();
+				this.$router.push(`/thanks/${order}`);
+			}
 		},
 	},
 };
--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import store from "./store";
 import router from "./router";
+import Vuelidate from "vuelidate";
 Vue.filter("currency", (value) =>
   new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(
     value
   )
 );
+Vue.use(Vuelidate);
 new Vue({
   render: (h) => h(App),
   store,
